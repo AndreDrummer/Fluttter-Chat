bc86446b0cfc0ef6d64026b40be8fccd1367e7b9
André Felipe <academicworks.adf@gmail.com>
2021-09-16 07:35:12 -0300

    feat(Model): complete chat model code and start connection with the server

---
+++ lib/core/model/connection/connector.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'package:flutter_socket_io/socket_io_manager.dart';
+
+String serverURL = 'http://192.168.9.42';
+
+late SocketIO _io;
--- lib/core/model/model.dart
+++ lib/core/model/model.dart
@@ -8,7 +8,7 @@ class FlutterChatModel extends Model {
 
   Directory? docsDir;
 
-  String gretting = '';
+  String greeting = '';
 
   String userName = '';
 
@@ -16,7 +16,7 @@ class FlutterChatModel extends Model {
 
   String currentRoomName = defaultRoomName;
 
-  List currenctRoomUserList = [];
+  List currentRoomUserList = [];
 
   bool currentRoomEnabled = false;
 
@@ -28,5 +28,104 @@ class FlutterChatModel extends Model {
 
   bool creatorFunctionsEnabled = false;
 
-  Map roomInvittes = {};
+  Map roomInvites = {};
+
+  void setGreeting(final String inGreeting) {
+    debugPrint('## FlutterChatModel.setGreeting(): inGreeting = $inGreeting');
+
+    greeting = inGreeting;
+    notifyListeners();
+  }
+
+  void setUserName(final String inUserName) {
+    debugPrint('## FlutterChatModel.setUserName(): inUserName = $inUserName');
+
+    userName = inUserName;
+    notifyListeners();
+  }
+
+  void setCurrentRoomName(final String inRoomName) {
+    debugPrint(
+        '## FlutterChatModel.setCurrentRoomName(): inRoomName = $inRoomName');
+
+    currentRoomName = inRoomName;
+    notifyListeners();
+  }
+
+  void setCreatorFunctionsEnabled(final bool inEnabled) {
+    debugPrint(
+        '## FlutterChatModel.setCreatorFunctionsEnabled(): inEnabled = $inEnabled');
+
+    creatorFunctionsEnabled = inEnabled;
+    notifyListeners();
+  }
+
+  void setCurrentRoomEnabled(final bool inEnabled) {
+    debugPrint(
+        '## FlutterChatModel.setCurrentRoomEnabled(): inEnabled = $inEnabled');
+
+    currentRoomEnabled = inEnabled;
+    notifyListeners();
+  }
+
+  void addMessage(final String inUsername, final String inMessage) {
+    currentRoomMessages.add({
+      'userName': inUsername,
+      'message': inMessage,
+    });
+
+    notifyListeners();
+  }
+
+  void setRoomList(final Map inRoomList) {
+    List rooms = [];
+    for (String roomName in inRoomList.keys) {
+      Map room = inRoomList[roomName];
+      rooms.add(room);
+    }
+
+    roomList = rooms;
+    notifyListeners();
+  }
+
+  void setUserList(final Map inUserList) {
+    debugPrint('## FlutterChatModel.setUserList(): inUserList = $inUserList');
+
+    List users = [];
+    for (String userName in inUserList.keys) {
+      Map user = inUserList[userName];
+      users.add(user);
+    }
+
+    userList = users;
+    notifyListeners();
+  }
+
+  void setCurrentRoomUserList(final Map inUserList) {
+    debugPrint(
+        '## FlutterChatModel.setCurrentRoomUserList(): inUserList = $inUserList');
+
+    List users = [];
+    for (String userName in inUserList.keys) {
+      Map user = inUserList[userName];
+      users.add(user);
+    }
+
+    currentRoomUserList = users;
+    notifyListeners();
+  }
+
+  void addRoomInvitte(final String inRoomName) {
+    roomInvites[inRoomName] = true;
+  }
+
+  void removeRoomInvite(final String inRoomName) {
+    roomInvites.remove(inRoomName);
+  }
+
+  void clearCurrentRoomMessages() {
+    currentRoomMessages.clear();
+  }
+
+  FlutterChatModel flutterChatModel = FlutterChatModel();
 }
